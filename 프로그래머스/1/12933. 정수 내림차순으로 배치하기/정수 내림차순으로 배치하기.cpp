934c78b5378d548e15598f0916a1e4dc6cfb216a
Lee_gw <89513932+Lgw2945@users.noreply.github.com>
2025-01-09 09:55:46 +0900

    Update 정수 내림차순으로 배치하기.cpp

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -16,3 +16,18 @@ long long solution(long long n) {
     
     return answer = stoll(a);
 }
+
+--- or ---
+
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+
+    string a = to_string(n);
+    sort(a.begin(), a.end(), greater<char>());
+    return answer = stoll(a);
+}
