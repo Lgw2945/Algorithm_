3b9af644ab223cdd4b53bfd4817c1ce3ea1fcdd7
Lee_gw <89513932+Lgw2945@users.noreply.github.com>
2025-02-06 09:02:27 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -1,19 +1,20 @@
-#include <string>
-#include <vector>
+  #include <string>
+    #include <vector>
 
-using namespace std;
+    using namespace std;
 
-bool solution(string s) {
-    if(s.length() != 4 && s.length() != 6)
-    {
-        return false;
-    }
-    for(char c : s)
-    {
-        if(!isdigit(c))
+    bool solution(string s) {
+        if(s.length() != 4 && s.length() != 6)
         {
             return false;
         }
+
+        for(char c : s)
+        {
+            if(!isdigit(c))
+            {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
