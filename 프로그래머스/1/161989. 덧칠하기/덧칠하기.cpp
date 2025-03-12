ea28d9f0e9594e374a4ba582ab15de749745e286
Lee_gw <89513932+Lgw2945@users.noreply.github.com>
2025-03-12 09:46:24 +0900

    [level 1] Title: 덧칠하기, Time: 0.32 ms, Memory: 5.77 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int current = 0;
+    
+    for (int i = 0; i < section.size(); i++) {
+        if (section[i] > current) {
+            answer++;
+            current = section[i] + m - 1;
+        }
+    }
+    return answer;
+}
